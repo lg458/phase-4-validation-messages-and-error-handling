fb77b3731e18560ea50cfe4e9a247abc7d50abfa
Laura Gonzalez <lg458@cornell.edu>
2022-09-06 01:29:13 -0700

    commit tree 023f5a9662996649f09dd396f5a1b5af76b8018a

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
+
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -23,8 +25,16 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+      .then((res) => {
+        if (res.ok) {
+          res.json().then((newMovie) => console.log(newMovie))
+        } else {
+          res.json().then((errorData) => { 
+            setErrors(errorData.errors)
+            console.log(errors)
+          })
+        }
+      })
   }
 
   function handleChange(e) {
@@ -125,6 +135,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
